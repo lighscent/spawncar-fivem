2d52031f8aef53687fc5cea2589c37b31e3acf5d
Азукиов <93555197+Azukiov@users.noreply.github.com>
2023-08-13 23:34:24 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,8 @@ games {'gta5'}
 
 author 'Dev by Azukiov'
 description 'SpawnCar'
-version '1.0.0'
+version '1.0'
 
--- ui_page "ui/index.html"
--- files {
-	
--- }
 
 client_scripts {
 	"client.lua",
